5c35de90cadf1e78f5fbd37d48d66675eca6537b
Jamar-Washington <jamarxwashington@gmail.com>
2022-11-21 22:55:28 -0600

    Updated R script with the T-tests included

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,5 +18,23 @@ summary(mpg_lm)
 ## Deliverable 2
 # Import and read Suspension_Coil.csv as a data frame
 susp_coil <- read.csv(file = "Suspension_Coil.csv")
-# View new dataframe to make sure it is read correctly
-View(susp_coil)
+
+# Create a total_summary dataframe using the summarize()
+# function to get the mean, median, variance, and std. dev.
+# of the suspension coil’s PSI column
+total_summary <- susp_coil %>% summarize(
+  Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+# Create a lot_summary dataframe using the group_by()
+# and the summarize() functions to group each manufacturing lot
+# by the mean, median, variance, and standard deviation of the 
+# suspension coil’s PSI column
+lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(
+  Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+## Deliverable 3
+# write an RScript using the t.test() function 
+# to determine if the PSI across all manufacturing lots
+# is statistically different from the population 
+# mean of 1,500 pounds per square inch
+t.test(susp_coil$PSI,mu=mean(susp_coil$PSI))
